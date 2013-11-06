f3c66830df024975f6b10a9ec8731fa108aa69d3
leison <leisonkennedy@gmail.com>
2013-11-07 00:28:35 +0300

    styled the story board

---
+++ story_board.php
@@ -0,0 +1,42 @@
+<?php include('header.php'); ?>
+    <div id="content">
+
+      <div class="container headerMarginTop">
+			<div class="story_board_main">
+            	<h1>RebelsMarket is the No. 1 counterculture community and marketplace in the world</h1>
+                <h2>Why should i join?</h2>
+                <div class="boxMin clearfix">
+                	<div class="discBox clearfix">
+                    	<div class="iconSide"><img src="img/followIcon.png" /></div>
+                        <div class="discDesc">
+                        	<h3>Follow</h3>
+                            <p>you can follow People whose style inspired you!</p>
+                        </div>
+                    </div>
+                    
+                    <div class="discBox clearfix">
+                    	<div class="iconSide"><img src="img/de_search_icon.png" /></div>
+                        <div class="discDesc">
+                        	<h3>Follow</h3>
+                            <p>you can follow People whose style inspired you!</p>
+                        </div>
+                    </div>
+                    
+                    <div class="discBox clearfix">
+                    	<div class="iconSide"><img src="img/deLoveIcon.png" /></div>
+                        <div class="discDesc">
+                        	<h3>Follow</h3>
+                            <p>you can follow People whose style inspired you!</p>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="btnCenter">
+             		 <a href="#" class="getStart">Get Started</a>
+           		 </div>
+               
+            </div>        
+      </div><!-- end of .container -->
+
+    </div><!-- end of #content -->
+<?php include('footer.php'); ?>
