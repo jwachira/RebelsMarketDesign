977f4187618ddf2dd52653b3edf11390b2725358
leison <leisonkennedy@gmail.com>
2014-04-07 22:59:18 +0300

    added clothing shop.php

---
+++ clothing-shop.php
@@ -0,0 +1,162 @@
+<?php include('header.php'); ?>
+<div id="content">
+
+<div class="container">
+  
+<section class="category-page">
+   <h1 class="page-title">Clothing Shop</h1>   
+   <div class="category-banners">
+       <div class="left-banners">
+           <figure>
+              <img src="img/Main_Category_Landing_Pages.jpg" alt="">
+              <figcaption>
+                 <div class="copy-text">
+                   <h3>
+                     Tattoo Tank starting at $15
+                   </h3>
+                   <a href="" class="shop-button">SHOP NOW</a>
+                 </div>
+              </figcaption>
+           </figure>
+
+            <figure>
+              <img src="img/Main_Category_Landing_Pages.jpg" alt="">
+              <figcaption>
+                 <div class="copy-text">
+                   <h3>
+                     Tattoo Tank starting at $15
+                   </h3>
+                   <a href="" class="shop-button">SHOP NOW</a>
+                 </div>
+              </figcaption>
+           </figure>
+
+       </div>
+       <div class="center-banners">
+          <figure>
+              <img src="img/main-banner.jpg" alt="">
+              <figcaption>
+                 <div class="copy-text">
+                 
+                   <a href="" class="shop-button">SHOP Men</a>
+                   <a href="" class="shop-button">SHOP Womens</a>
+                 </div>
+              </figcaption>
+           </figure>
+          
+       </div>
+
+
+       <div class="right-banners">
+            <figure>
+              <img src="img/Main_Category_Landing_Pages.jpg" alt="">
+              <figcaption>
+                 <div class="copy-text">
+                   <h3>
+                     Tattoo Tank starting at $15
+                   </h3>
+                   <a href="" class="shop-button">SHOP NOW</a>
+                 </div>
+              </figcaption>
+           </figure>
+
+            <figure>
+              <img src="img/Main_Category_Landing_Pages.jpg" alt="">
+              <figcaption>
+                 <div class="copy-text">
+                   <h3>
+                     Tattoo Tank starting at $15
+                   </h3>
+                   <a href="" class="shop-button">SHOP NOW</a>
+                 </div>
+              </figcaption>
+           </figure>
+       </div>
+
+   </div> 
+
+  <div class="main-content">
+    <aside class="side-bar">
+        <h4>WOMEN'S CLOTHING</h4>
+        <div class="verticle-menu new-items">
+            <ul>
+                <li><a href="">Undies/Thongs/panties</a></li>
+                <li><a href="">Consumables</a></li>
+                <li><a href="">Keychains</a></li>
+                <li><a href="">Necklaces</a></li>
+                <li><a href="">Bar &amp; Glasssware</a></li>
+                <li><a href="">Tees</a></li>
+                <li><a href="">Bracelets &amp; Wristbands</a></li>
+            </ul>
+        </div>
+
+        <h4>MEN'S CLOTHING</h4>
+        <div class="verticle-menu new-items">
+            <ul>
+                <li><a href="">Undies/Thongs/panties</a></li>
+                <li><a href="">Consumables</a></li>
+                <li><a href="">Keychains</a></li>
+                <li><a href="">Necklaces</a></li>
+                <li><a href="">Bar &amp; Glasssware</a></li>
+                <li><a href="">Tees</a></li>
+                <li><a href="">Bracelets &amp; Wristbands</a></li>
+            </ul>
+        </div>
+
+        <h4>FEATURED SHOPS</h4>
+        <div class="verticle-menu new-items">
+            <ul>
+                <li><a href="">Undies/Thongs/panties</a></li>
+                <li><a href="">Consumables</a></li>
+                <li><a href="">Keychains</a></li>
+                <li><a href="">Necklaces</a></li>
+                <li><a href="">Bar &amp; Glasssware</a></li>
+                <li><a href="">Tees</a></li>
+                <li><a href="">Bracelets &amp; Wristbands</a></li>
+            </ul>
+        </div>
+
+        <h4>SHOP BY STYLE</h4>
+        <div class="verticle-menu new-items">
+            <ul>
+                <li><a href="">Undies/Thongs/panties</a></li>
+                <li><a href="">Consumables</a></li>
+                <li><a href="">Keychains</a></li>
+                <li><a href="">Necklaces</a></li>
+                <li><a href="">Bar &amp; Glasssware</a></li>
+                <li><a href="">Tees</a></li>
+                <li><a href="">Bracelets &amp; Wristbands</a></li>
+            </ul>
+        </div>
+
+        <h4>Other Categories</h4>
+        <div class="verticle-menu new-items">
+            <ul>
+                <li><a href="">Undies/Thongs/panties</a></li>
+                <li><a href="">Consumables</a></li>
+                <li><a href="">Keychains</a></li>
+                <li><a href="">Necklaces</a></li>
+                <li><a href="">Bar &amp; Glasssware</a></li>
+                <li><a href="">Tees</a></li>
+                <li><a href="">Bracelets &amp; Wristbands</a></li>
+            </ul>
+        </div>
+
+    </aside>
+
+    <section class="category-block">
+       <h4 class="category-title">
+         Top Trending Clothing <a href="">View All Items</a>
+       </h4>
+    </section>
+
+  </div>
+
+</section>
+
+
+</div><!-- end of .container -->
+
+</div><!-- end of #content -->
+<?php include('footer.php'); ?>	
+
