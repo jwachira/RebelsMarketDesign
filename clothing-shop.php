b5ad9792e39d57d943a2b253f9efd83cebf4a371
leison <leisonkennedy@gmail.com>
2014-04-08 16:50:45 +0300

    added cloth landing.php

--- clothing-shop.php
+++ clothing-shop.php
@@ -1,4 +1,13 @@
 <?php include('header.php'); ?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <div id="content">
 
 <div class="container">
@@ -142,12 +151,345 @@
             </ul>
         </div>
 
+      <div class="like-btn">
+          <div class="fb-like-box" data-href="https://www.facebook.com/FacebookDevelopers" data-width="220" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="false"></div>
+      </div>
+
     </aside>
 
     <section class="category-block">
        <h4 class="category-title">
          Top Trending Clothing <a href="">View All Items</a>
        </h4>
+       <div class="product-div">
+         
+           <div class="single-cloth">
+            <figure class="main-pic">
+              <a href=""><img src="img/product-sample.jpg" alt=""></a>
+            </figure>
+            <div class="product-excerpt">
+                <a href="" class="shop-img"><img src="img/seller-avatar.jpg" alt=""></a>
+                <div class="left-text">
+                    <a class="product-id" href="">Skeleton hand spiked bra</a>
+                    <a class="seller-id"  href="">By Rachels graveyard</a>
+                </div>
+            </div>
+          </div>
+
+           <div class="single-cloth">
+            <figure class="main-pic">
+              <a href=""><img src="img/product-sample.jpg" alt=""></a>
+            </figure>
+            <div class="product-excerpt">
+                <a href="" class="shop-img"><img src="img/seller-avatar.jpg" alt=""></a>
+                <div class="left-text">
+                    <a class="product-id" href="">Skeleton hand spiked bra</a>
+                    <a class="seller-id"  href="">By Rachels graveyard</a>
+                </div>
+            </div>
+          </div>
+
+           <div class="single-cloth">
+            <figure class="main-pic">
+              <a href=""><img src="img/product-sample.jpg" alt=""></a>
+            </figure>
+            <div class="product-excerpt">
+                <a href="" class="shop-img"><img src="img/seller-avatar.jpg" alt=""></a>
+                <div class="left-text">
+                    <a class="product-id" href="">Skeleton hand spiked bra</a>
+                    <a class="seller-id"  href="">By Rachels graveyard</a>
+                </div>
+            </div>
+          </div>
+
+           <div class="single-cloth">
+            <figure class="main-pic">
+              <a href=""><img src="img/product-sample.jpg" alt=""></a>
+            </figure>
+            <div class="product-excerpt">
+                <a href="" class="shop-img"><img src="img/seller-avatar.jpg" alt=""></a>
+                <div class="left-text">
+                    <a class="product-id" href="">Skeleton hand spiked bra</a>
+                    <a class="seller-id"  href="">By Rachels graveyard</a>
+                </div>
+            </div>
+          </div>
+
+           <div class="single-cloth">
+            <figure class="main-pic">
+              <a href=""><img src="img/product-sample.jpg" alt=""></a>
+            </figure>
+            <div class="product-excerpt">
+                <a href="" class="shop-img"><img src="img/seller-avatar.jpg" alt=""></a>
+                <div class="left-text">
+                    <a class="product-id" href="">Skeleton hand spiked bra</a>
+                    <a class="seller-id"  href="">By Rachels graveyard</a>
+                </div>
+            </div>
+          </div>
+
+           <div class="single-cloth">
+            <figure class="main-pic">
+              <a href=""><img src="img/product-sample.jpg" alt=""></a>
+            </figure>
+            <div class="product-excerpt">
+                <a href="" class="shop-img"><img src="img/seller-avatar.jpg" alt=""></a>
+                <div class="left-text">
+                    <a class="product-id" href="">Skeleton hand spiked bra</a>
+                    <a class="seller-id"  href="">By Rachels graveyard</a>
+                </div>
+            </div>
+          </div>
+
+           <div class="single-cloth">
+            <figure class="main-pic">
+              <a href=""><img src="img/product-sample.jpg" alt=""></a>
+            </figure>
+            <div class="product-excerpt">
+                <a href="" class="shop-img"><img src="img/seller-avatar.jpg" alt=""></a>
+                <div class="left-text">
+                    <a class="product-id" href="">Skeleton hand spiked bra</a>
+                    <a class="seller-id"  href="">By Rachels graveyard</a>
+                </div>
+            </div>
+          </div>
+
+           <div class="single-cloth">
+            <figure class="main-pic">
+              <a href=""><img src="img/product-sample.jpg" alt=""></a>
+            </figure>
+            <div class="product-excerpt">
+                <a href="" class="shop-img"><img src="img/seller-avatar.jpg" alt=""></a>
+                <div class="left-text">
+                    <a class="product-id" href="">Skeleton hand spiked bra</a>
+                    <a class="seller-id"  href="">By Rachels graveyard</a>
+                </div>
+            </div>
+          </div>
+
+           <div class="single-cloth">
+            <figure class="main-pic">
+              <a href=""><img src="img/product-sample.jpg" alt=""></a>
+            </figure>
+            <div class="product-excerpt">
+                <a href="" class="shop-img"><img src="img/seller-avatar.jpg" alt=""></a>
+                <div class="left-text">
+                    <a class="product-id" href="">Skeleton hand spiked bra</a>
+                    <a class="seller-id"  href="">By Rachels graveyard</a>
+                </div>
+            </div>
+          </div>
+
+          <div class="single-cloth">
+            <figure class="main-pic">
+              <a href=""><img src="img/product-sample.jpg" alt=""></a>
+            </figure>
+            <div class="product-excerpt">
+                <a href="" class="shop-img"><img src="img/seller-avatar.jpg" alt=""></a>
+                <div class="left-text">
+                    <a class="product-id" href="">Skeleton hand spiked bra</a>
+                    <a class="seller-id"  href="">By Rachels graveyard</a>
+                </div>
+            </div>
+          </div>
+
+          <div class="single-cloth">
+            <figure class="main-pic">
+              <a href=""><img src="img/product-sample.jpg" alt=""></a>
+            </figure>
+            <div class="product-excerpt">
+                <a href="" class="shop-img"><img src="img/seller-avatar.jpg" alt=""></a>
+                <div class="left-text">
+                    <a class="product-id" href="">Skeleton hand spiked bra</a>
+                    <a class="seller-id"  href="">By Rachels graveyard</a>
+                </div>
+            </div>
+          </div>
+
+          <div class="single-cloth">
+            <figure class="main-pic">
+              <a href=""><img src="img/product-sample.jpg" alt=""></a>
+            </figure>
+            <div class="product-excerpt">
+                <a href="" class="shop-img"><img src="img/seller-avatar.jpg" alt=""></a>
+                <div class="left-text">
+                    <a class="product-id" href="">Skeleton hand spiked bra</a>
+                    <a class="seller-id"  href="">By Rachels graveyard</a>
+                </div>
+            </div>
+          </div>
+
+       </div>
+
+       <h4 class="category-title">
+         Latest Clothing Reviews <a href="">View All Items</a>
+       </h4>
+       <div class="latest-review">
+          
+          <div class="review-column">
+              <div class="review-left">
+                <figure><a href=""><img src="img/review-icon.jpg" alt=""></a></figure>
+                <p>
+                  January 11,2014 <br> United States
+                </p>
+              </div>
+              <div class="review-right">
+                 <h3>
+                    <a href="">Full Hand Metal Skeleton Bracelet..</a>
+                    <span>US 46.99</span>
+                 </h3>
+                 <h5><a href="">Niente di speciale</a></h5>
+                 <p>
+                   É veramente un bel accessorio, ma purtroppo il materiale di fabbricazione é veramente scarso, non é abbastanza resistente. Sarebbe ottimo se fosse stato costruito con materiale e metodi più resistenti!
+                 </p>
+              </div>
+          </div>
+
+          <div class="review-column">
+              <div class="review-left">
+                <figure><a href=""><img src="img/review-icon.jpg" alt=""></a></figure>
+                <p>
+                  January 11,2014 <br> United States
+                </p>
+              </div>
+              <div class="review-right">
+                 <h3>
+                    <a href="">Full Hand Metal Skeleton Bracelet..</a>
+                    <span>US 46.99</span>
+                 </h3>
+                 <h5><a href="">Niente di speciale</a></h5>
+                 <p>
+                   É veramente un bel accessorio, ma purtroppo il materiale di fabbricazione é veramente scarso, non é abbastanza resistente. Sarebbe ottimo se fosse stato costruito con materiale e metodi più resistenti!
+                 </p>
+              </div>
+          </div>
+  
+       </div>
+
+       <div class="latest-review">
+          
+          <div class="review-column">
+              <div class="review-left">
+                <figure><a href=""><img src="img/review-icon.jpg" alt=""></a></figure>
+                <p>
+                  January 11,2014 <br> United States
+                </p>
+              </div>
+              <div class="review-right">
+                 <h3>
+                    <a href="">Full Hand Metal Skeleton Bracelet..</a>
+                    <span>US 46.99</span>
+                 </h3>
+                 <h5><a href="">Niente di speciale</a></h5>
+                 <p>
+                   É veramente un bel accessorio, ma purtroppo il materiale di fabbricazione é veramente scarso, non é abbastanza resistente. Sarebbe ottimo se fosse stato costruito con materiale e metodi più resistenti!
+                 </p>
+              </div>
+          </div>
+
+          <div class="review-column">
+              <div class="review-left">
+                <figure><a href=""><img src="img/review-icon.jpg" alt=""></a></figure>
+                <p>
+                  January 11,2014 <br> United States
+                </p>
+              </div>
+              <div class="review-right">
+                 <h3>
+                    <a href="">Full Hand Metal Skeleton Bracelet..</a>
+                    <span>US 46.99</span>
+                 </h3>
+                 <h5><a href="">Niente di speciale</a></h5>
+                 <p>
+                   É veramente un bel accessorio, ma purtroppo il materiale di fabbricazione é veramente scarso, non é abbastanza resistente. Sarebbe ottimo se fosse stato costruito con materiale e metodi più resistenti!
+                 </p>
+              </div>
+          </div>
+  
+       </div>
+
+        <div class="latest-review no-love">
+          
+          <div class="review-column">
+              <div class="review-left">
+                <figure><a href=""><img src="img/review-icon.jpg" alt=""></a></figure>
+                <p>
+                  January 11,2014 <br> United States
+                </p>
+              </div>
+              <div class="review-right">
+                 <h3>
+                    <a href="">Full Hand Metal Skeleton Bracelet..</a>
+                    <span>US 46.99</span>
+                 </h3>
+                 <h5><a href="">Niente di speciale</a></h5>
+                 <p>
+                   É veramente un bel accessorio, ma purtroppo il materiale di fabbricazione é veramente scarso, non é abbastanza resistente. Sarebbe ottimo se fosse stato costruito con materiale e metodi più resistenti!
+                 </p>
+              </div>
+          </div>
+
+          <div class="review-column">
+              <div class="review-left">
+                <figure><a href=""><img src="img/review-icon.jpg" alt=""></a></figure>
+                <p>
+                  January 11,2014 <br> United States
+                </p>
+              </div>
+              <div class="review-right">
+                 <h3>
+                    <a href="">Full Hand Metal Skeleton Bracelet..</a>
+                    <span>US 46.99</span>
+                 </h3>
+                 <h5><a href="">Niente di speciale</a></h5>
+                 <p>
+                   É veramente un bel accessorio, ma purtroppo il materiale di fabbricazione é veramente scarso, non é abbastanza resistente. Sarebbe ottimo se fosse stato costruito con materiale e metodi più resistenti!
+                 </p>
+              </div>
+          </div>
+  
+       </div>
+
+       <h4 class="category-title">
+         Recent Blog Post<a href="">View All Items</a>
+       </h4>
+       <div class="blog-home">
+          
+          <div class="blog-column">
+            <figure>
+              <a href=""><img src="img/blog-placeholder.jpg" alt=""></a>
+            </figure>
+            <div class="blog-excerpt">
+                <h4><a href="">A Love Letter to Mascara</a></h4>
+                <p>
+                  Essentially, the thinking is that using fake lashes in mascara ads leads women on to thinking that their results will be exactly like Freja Beha Erichsen’s. I understand this concern... <a href="">read more</a>
+                </p>
+            </div>
+          </div>
+
+          <div class="blog-column">
+            <figure>
+              <a href=""><img src="img/blog-placeholder.jpg" alt=""></a>
+            </figure>
+            <div class="blog-excerpt">
+                <h4><a href="">A Love Letter to Mascara</a></h4>
+                <p>
+                  Essentially, the thinking is that using fake lashes in mascara ads leads women on to thinking that their results will be exactly like Freja Beha Erichsen’s. I understand this concern... <a href="">read more</a>
+                </p>
+            </div>
+          </div>
+
+       </div>
+
+        <h4 class="category-title">About Clothing</h4>
+        <div class="home-about-section">
+          <p>Clothing has been a staple of human civilization since the first caveman invented the loincloth.</p>
+          <p>            
+Nowadays, you don't have to slay a mighty mastodon to improve your wardrobe. In fact, you don't even have to leave the comfort of your home! Zappos makes it easy with free shipping and a 365-day return policy, so kick your feet up and enjoy the twenty-first-century shopping experience. The sheer amount of options is dazzling.
+          </p>
+        </div>
+
+      
     </section>
 
   </div>
